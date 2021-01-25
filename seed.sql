58648268e365270cf01ea7ac84981effd7194770
Kristine J Horton <70182286+Kristine-1977@users.noreply.github.com>
2021-01-25 16:09:00 -0700

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,22 @@
+-- Creates new rows containing data in all named columns --
+INSERT INTO Department (name)
+VALUES("Human Resources");
+
+INSERT INTO Department (name)
+VALUES("Finance");
+
+INSERT INTO Role(title, salary, department_id)
+VALUES ("Human Resource Coordinator", 75000, 1)
+VALUES("Accountant", 50000, 2)
+VALUES("Cashier", 25000, 2)
+
+INSERT INTO Role (name)
+VALUES ("Human Resource Coordinator");
+
+INSERT INTO Role (name)
+VALUES ("Accountant");I
+
+INSERT INTO Role (name)
+VALUES ("Cashier");
+
+
