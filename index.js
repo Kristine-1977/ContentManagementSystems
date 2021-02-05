d208b70875001d7b5bcbea89c5dbcf1a8b831caa
Kristine J Horton <70182286+Kristine-1977@users.noreply.github.com>
2021-02-04 22:59:49 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-//const consoleTable = require("console.Table")
+const consoleTable = require("console.table")
 
 const connection = mysql.createConnection({
     multipleStatements: true,
     host: "localhost",
-
-    // Your port; if not 3306
     port: 3306,
-
-    // Your username
     user: "root",
-
-    // Your password
     password: "#257976QXZIcjkg",
     database: "EmployeeDB"
 });
 
 connection.connect(function (err) {
-    console.log('hello');
+    console.log("hello");
     if (err) throw err;
-   // runSearch();
+    runSearch();
 });
 
 function runSearch() {
@@ -30,188 +24,89 @@ function runSearch() {
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "Find employees by role",
-                "Find employees by department",
-                "Find employees by name",
-                "What employee would you like to remove",
-                "View employees by manager",
-                "Update employee role",
-                "Update manager role",
-                "Add employee",
-                "Update employee role"
+                "View All Employees",
+                "View All Employees by Department",
+                "View All Employees by Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee",
+                "Exit"
             ]
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "Find employee by role":
-                    employeeSearch();
+        },
+        ).then(answers => {
+            switch (answers.choice) {
+                case "View All Employees":
+                    allEmployees();
                     break;
-
-                case "Find employees by department":
-                    employeeSearch();
+                case "View All Employees by Department":
+                    byDepartment();
                     break;
-
-                case "Find employee by name":
-                    employeeSearch();
+                case "View All Employees by Manager":
+                    byManager();
                     break;
-
-                case "What employee would you like to remove":
-                    employeeSearch();
+                case "Add Employee":
+                    addEmployee();
                     break;
-
-                case "View employees by manager":
-                    employeeSearch();
+                case "Remove Employee":
+                    removeEmployee();
                     break;
-
-                case "Update employee role":
-                    employeeSearch();
+                case "Update Employee":
+                    updateEmployee();
                     break;
-
-                case "Update manager role":
-                    employeeSearch();
-                    break;
-
-                case "Add employee":
-                    employeeSearch();
-                    break;
-
-                case "Update employee role":
-                    employeeSearch();
+                // Exit
+                case "Exit":
+                    console.log("You're done!");
                     break;
             }
-        });
-}
-/**
- function artistSearch() {
-    inquirer
-        .prompt({
-            name: "artist",
-            type: "input",
-            message: "What artist would you like to search for?"
         })
-        .then(function (answer) {
-            var query = "SELECT position, song, year FROM top5000 WHERE ?";
-            connection.query(query, { artist: answer.artist }, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
-                }
-                runSearch();
-            });
-        });
-}
+};
 
-function multiSearch() {
-    var query = "SELECT employee FROM top5000 GROUP BY employee HAVING count(*) > 1";
-    connection.query(query, function (err, res) {
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i].employee);
-        }
+function allEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res)
         runSearch();
     });
 }
 
-function rangeSearch() {
-    inquirer
-        .prompt([
+
+function addEmployee() {
+    connection.query("Select * FROM role", function (err, res) {
+        console.log(res);
+        inquirer.prompt([
             {
-                name: "start",
-                type: "input",
-                message: "Enter starting position: ",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
+                type: 'input',
+                name: 'firstName',
+                message: "What is your employee's first name?"
             },
             {
-                name: "end",
-                type: "input",
-                message: "Enter ending position: ",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
+                type: 'input',
+                name: 'lastName',
+                message: "What is your employee's last name?"
+            },
+            {
+                type: 'input',
+                name: 'managersid',
+                message: "What is your manager's id?"
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: "What is your employee's role?",
+                choices: res.map(item => ({ name: item.title, value: item.id }))
             }
-        ])
-        .then(function (answer) {
-            var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-            connection.query(query, [answer.start, answer.end], function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log(
-                        "Position: " +
-                        res[i].position +
-                        " || Song: " +
-                        res[i].song +
-                        " || Artist: " +
-                        res[i].artist +
-                        " || Year: " +
-                        res[i].year
-                    );
-                }
+        ]).then(answers => {
+            console.log(answers);
+            var employeeData = {
+                manager_id: answers.managersid,
+                role_id: answers.role, Last_Name: answers.lastName, First_Name: answers.firstName
+            }
+            connection.query("INSERT INTO employee set ?", employeeData, function (err, res) {
+                if (err) throw err;
+                console.table(res)
                 runSearch();
             });
         });
+
+    });
 }
-
-function songSearch() {
-    inquirer
-        .prompt({
-            name: "song",
-            type: "input",
-            message: "What song would you like to look for?"
-        })
-        .then(function (answer) {
-            console.log(answer.song);
-            connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function (err, res) {
-                console.log(
-                    "Position: " +
-                    res[0].position +
-                    " || Song: " +
-                    res[0].song +
-                    " || Artist: " +
-                    res[0].artist +
-                    " || Year: " +
-                    res[0].year
-                );
-                runSearch();
-            });
-        });
-}
-
-function songAndAlbumSearch() {
-    inquirer
-        .prompt({
-            name: "artist",
-            type: "input",
-            message: "What artist would you like to search for?"
-        })
-        .then(function (answer) {
-            var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-            query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-            query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-            connection.query(query, [answer.artist, answer.artist], function (err, res) {
-                console.log(res.length + " matches found!");
-                for (var i = 0; i < res.length; i++) {
-                    console.log(
-                        i + 1 + ".) " +
-                        "Year: " +
-                        res[i].year +
-                        " Album Position: " +
-                        res[i].position +
-                        " || Artist: " +
-                        res[i].artist +
-                        " || Song: " +
-                        res[i].song +
-                        " || Album: " +
-                        res[i].album
-                    );
-                }
-
-                runSearch();
-            });
-        });
-}*/
